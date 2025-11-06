5476a13bf2424eff5e39e1d767dea188e700fe9d
Taha Yılmaz <mtahayilmz@gmail.com>
2025-11-06 12:42:21 +0300

    Part 1 -Complete

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// Safe Area Context'ten yeni SafeAreaView'i içe aktarıyoruz
+import { SafeAreaView } from 'react-native-safe-area-context'; 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // SafeAreaView'e container stili (flex: 1 ve arkaplan rengi) uygulanır [cite: 49, 50]
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text>My Profile App</Text> {/* [cite: 43] */}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // Tüm ekranı kaplamak için flex: 1 [cite: 49]
+    flex: 1, 
+    // Hafif arka plan rengi [cite: 50]
+    backgroundColor: '#f0f2f5', 
+  },
+  content: {
+    // View bileşenine dolgu (padding) ekleme [cite: 51]
+    padding: 20, 
   },
 });
